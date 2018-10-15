9676bcff677150347b4bea1065c378f51eb7a72a
bjorndragland <bjorndragland@hotmail.com>
2018-10-15 22:11:30 +0200

    committer database-fil

---
+++ config/database.php
@@ -0,0 +1,27 @@
+<?php
+ini_set('default_charset', 'utf-8');
+class Database{
+ 
+    // specify your own database credentials
+    private $host = "localhost";
+    private $db_name = "kaerkis";
+    private $username = "root";
+    private $password = "";
+    public $conn;
+ 
+    // get the database connection
+    public function getConnection(){
+ 
+        $this->conn = null;
+ 
+        try{
+            $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
+            $this->conn->exec("set names utf8");
+        }catch(PDOException $exception){
+            echo "Connection error: " . $exception->getMessage();
+        }
+ 
+        return $this->conn;
+    }
+}
+?>
